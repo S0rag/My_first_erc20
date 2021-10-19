02470816c20f28dca8ec7881e9678e3aaef09e8b
sorag <sorag.eth@protonmail.com>
2021-10-19 10:08:08 +0200

    Fix for verif

--- scripts/1_deploy_token.py
+++ scripts/1_deploy_token.py
@@ -1,4 +1,4 @@
-from brownie import SoragToken
+from brownie import SoragToken, network, config
 from scripts.helpful_scripts import get_account
 from web3 import Web3
 
@@ -6,5 +6,9 @@ initial_supply = Web3.toWei(1000, "ether")
 
 def main():
     account = get_account()
-    my_token = SoragToken.deploy(initial_supply, {"from": account})
+    my_token = SoragToken.deploy(
+        initial_supply,
+        {"from": account},
+        publish_source=config["networks"][network.show_active()].get("verify")
+    )
     print(my_token.name())
